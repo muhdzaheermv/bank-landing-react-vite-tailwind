07fda16b605c863031092bb1438a3dead6d6e950
muhammed zaheer mv <136464749+muhdzaheermv@users.noreply.github.com>
2025-06-30 00:53:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Bank-reactvite-tailwind/",
+  base: "/bank-landing-react-vite-tailwind/",
   plugins: [react()],
+  base: './',
 });
